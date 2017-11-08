b4bebcc9b7237d7e2867ea8a4299fa1c39402293
Robert Corty <rcorty@gmail.com>
2017-11-08 11:29:02 -0500

    minor improvements
    Signed-off-by: Robert Corty <rcorty@gmail.com>

--- scripts/genome_scans_for_manuscript.R
+++ scripts/genome_scans_for_manuscript.R
@@ -1,15 +1,12 @@
-args <- commandArgs(trailingOnly = TRUE)
-focal.phen.name <- args[1]
-
 #### SIMULATE CROSS AND PHENOTYPES ####
 library(tidyr); library(dplyr); library(qtl); library(vqtl);
+
 set.seed(27599)
 
 # simulate the cross
 my.cross <- qtl::sim.cross(map = qtl::sim.map(len = rep(100, 3), n.mar = 11, eq.spacing = TRUE, include.x = FALSE, anchor.tel = TRUE),
 													 n.ind = 400,
 													 type = 'f2')
-my.cross$pheno$sex <- rep(x = c(0, 1), each = 200)
 my.cross$pheno$batch <- sample(x = 10, size = 400, replace = TRUE)
 my.cross <- qtl::calc.genoprob(cross = my.cross, step = 2)
 batch.effects <- runif(n = 10, min = -0.5, max = 0.5)
@@ -19,61 +16,103 @@ my.cross$pheno$phenotype1 <- rnorm(n = qtl::nind(my.cross),
 																	 mean = 0,
 																	 sd = 1)
 my.cross$pheno$phenotype2 <- rnorm(n = qtl::nind(my.cross),
-																	 mean = 0.28*(my.cross$geno$`1`$data[,6] - 2),
+																	 mean = 0.31*(my.cross$geno$`1`$data[,6] - 2),
 																	 sd = 1)
 my.cross$pheno$phenotype3 <- rnorm(n = qtl::nind(my.cross),
 																	 mean = 0,
-																	 sd = exp(0.23*(my.cross$geno$`2`$data[,6] - 2)))
+																	 sd = exp(0.2*(my.cross$geno$`2`$data[,6] - 2)))
 my.cross$pheno$phenotype4 <- rnorm(n = qtl::nind(my.cross),
-																	 mean = 0.24*(my.cross$geno$`3`$data[,6] - 2),
-																	 sd = exp(0.16*(my.cross$geno$`3`$data[,6] - 2)))
+																	 mean = 0.23*(my.cross$geno$`3`$data[,6] - 2),
+																	 sd = exp(0.17*(my.cross$geno$`3`$data[,6] - 2)))
 
 # simulate phenotype1x through phenotype4x, for the appendix
 my.cross$pheno$phenotype1x <- rnorm(n = qtl::nind(my.cross),
 																		mean = 0,
-																		sd = exp(my.cross$pheno$sex - 0.5))
-my.cross$pheno$phenotype2x <- rnorm(n = qtl::nind(my.cross),
-																		mean = 0.48*(my.cross$geno$`1`$data[,6] - 2),
 																		sd = exp(0.5*batch.effects[my.cross$pheno$batch]))
+my.cross$pheno$phenotype2x <- rnorm(n = qtl::nind(my.cross),
+																		mean = 0.45*(my.cross$geno$`1`$data[,6] - 2),
+																		sd = exp(batch.effects[my.cross$pheno$batch]))
 my.cross$pheno$phenotype3x <- rnorm(n = qtl::nind(my.cross),
 																		mean = 0,
-																		sd = exp(0.24*(my.cross$geno$`2`$data[,6] - 2) + 0.5*(batch.effects[my.cross$pheno$batch])))
+																		sd = exp(0.24*(my.cross$geno$`2`$data[,6] - 2) + (batch.effects[my.cross$pheno$batch])))
 my.cross$pheno$phenotype4x <- rnorm(n = qtl::nind(my.cross),
-																		mean = 0.21*(my.cross$geno$`3`$data[,6] - 2),
-																		sd = exp(0.12*(my.cross$geno$`3`$data[,6] - 2) + 0.2*(batch.effects[my.cross$pheno$batch])))
+																		mean = 0.22*(my.cross$geno$`3`$data[,6] - 2),
+																		sd = exp(0.15*(my.cross$geno$`3`$data[,6] - 2) + (batch.effects[my.cross$pheno$batch])))
 
 
-##### LOD SCORE GENOME SCAN ####
-so <- qtl::scanone(cross = my.cross,
-									 pheno.col = focal.phen.name,
-									 addcovar = my.cross$pheno$batch)
-sov <- scanonevar(cross = my.cross,
-									mean.formula = formula(paste(focal.phen.name, '~ batch + mean.QTL.add + mean.QTL.dom')),
-									var.formula = ~batch + var.QTL.add + var.QTL.dom)
-p1 <- plot(x = sov, y = so, ylim = c(0, 6))
-ggplot2::ggsave(plot = p1, height = 2.5, width = 9,
-								filename = paste0('images/LOD_scan_', focal.phen.name,'.pdf'))
-message('Done with so and sov')
+sos <- sovs <- list()
+for (focal.phen.name in c('phenotype1', 'phenotype2', 'phenotype3', 'phenotype4')) {
+	##### LOD SCORE GENOME SCAN ####
+	so <- qtl::scanone(cross = my.cross,
+										 pheno.col = focal.phen.name,
+										 addcovar = my.cross$pheno$batch)
+	sov <- scanonevar(cross = my.cross,
+										mean.formula = formula(paste(focal.phen.name, '~mean.QTL.add + mean.QTL.dom')),
+										var.formula = ~ var.QTL.add + var.QTL.dom)
+
+	#### PERMUTATIONS FOR EMPIRICAL P-VALUES ####
+	perms <-  qtl::scanone(cross = my.cross,
+												 pheno.col = focal.phen.name,
+												 n.perm = 1000,
+												 verbose = FALSE)
+	the.evd <- evd::fgev(x = perms)
+	so$empir.p <- evd::pgev(q = so$lod,
+													loc = fitted(the.evd)[1],
+													scale = fitted(the.evd)[2],
+													shape = fitted(the.evd)[3],
+													lower.tail = FALSE)
+
+	sov <- scanonevar.perm(sov = sov, n.perms = 1000, random.seed = 27599, n.cores = 40)
+
+	sos[[focal.phen.name]] <- so
+	sovs[[focal.phen.name]] <- sov
+
+	message('Done with permutations for ', focal.phen.name)
+
+}
 
 
-#### PERMUTATIONS FOR EMPIRICAL P-VALUES ####
-perms <-  qtl::scanone(cross = my.cross,
-											 pheno.col = focal.phen.name,
-											 addcovar = my.cross$pheno$batch,
-											 n.perm = 1000,
-											 verbose = FALSE)
-the.evd <- evd::fgev(x = perms)
-so$empir.p <- evd::pgev(q = so$lod,
-												loc = fitted(the.evd)[1],
-												scale = fitted(the.evd)[2],
-												shape = fitted(the.evd)[3],
-												lower.tail = FALSE)
-message('Done with so perms')
 
-sovp <- scanonevar.perm(sov = sov, n.perms = 1000, random.seed = 27599, n.cores = 40)
-p2 <- plot(x = sovp, y = so, ylim = c(0, 3.5))
-ggplot2::ggsave(plot = p2, height = 2.5, width = 9,
-								filename = paste0('images/empir_p_scan_', focal.phen.name,'.pdf'))
-message('Done with sov perms')
+soxs <- sovxs <- list()
+
+for (focal.phen.name in c('phenotype1x', 'phenotype2x', 'phenotype3x', 'phenotype4x')) {
+
+	##### LOD SCORE GENOME SCAN ####
+	so <- qtl::scanone(cross = my.cross,
+										 pheno.col = focal.phen.name,
+										 addcovar = my.cross$pheno$batch)
+	sov <- scanonevar(cross = my.cross,
+										mean.formula = formula(paste(focal.phen.name, '~ batch + mean.QTL.add + mean.QTL.dom')),
+										var.formula = ~batch + var.QTL.add + var.QTL.dom)
+	message('Done with ', focal.phen.name)
 
 
+	#### PERMUTATIONS FOR EMPIRICAL P-VALUES ####
+	perms <-  qtl::scanone(cross = my.cross,
+												 pheno.col = focal.phen.name,
+												 addcovar = my.cross$pheno$batch,
+												 n.perm = 1000,
+												 verbose = FALSE)
+	the.evd <- evd::fgev(x = perms)
+	so$empir.p <- evd::pgev(q = so$lod,
+													loc = fitted(the.evd)[1],
+													scale = fitted(the.evd)[2],
+													shape = fitted(the.evd)[3],
+													lower.tail = FALSE)
+
+	sov <- scanonevar.perm(sov = sov, n.perms = 1000, random.seed = 27599, n.cores = 40)
+
+	soxs[[focal.phen.name]] <- so
+	sovxs[[focal.phen.name]] <- sov
+
+	message('Done with permutations for ', focal.phen.name)
+
+}
+
+results <- list(sos = sos,
+								sovs = sovs,
+								soxs = soxs,
+								sovxs = sovxs)
+
+saveRDS(object = results,
+				file = paste0('scans_', Sys.time(),'.RDS'))
